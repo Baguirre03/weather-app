5eb28497a7475fb417d3d146f9b58a1f3236e629
Ben Aguirre <ben.aguirre03@gmail.com>
2023-06-04 14:52:12 -0500

    relocate data correctly and display forecast

--- src/index.js
+++ src/index.js
@@ -1,131 +1,112 @@
 import "./style.css";
-
-class Weather {
-  async defaultSearch() {
-    try {
-      const weatherData = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=61a80c65e68244318ae200949231005&q=new york`
-      );
-      const formattedData = await weatherData.json();
-      this.dataIntoObject(formattedData);
-    } catch (err) {
-      throw new Error(err);
-    }
-  }
-
-  async searchWeather(search) {
-    try {
-      const weatherData = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=61a80c65e68244318ae200949231005&q=${search}`
-      );
-      const formattedData = await weatherData.json();
-      this.dataIntoObject(formattedData);
-    } catch (err) {
-      this.defaultSearch();
-      throw new Error(err);
-    }
-  }
-
-  dataIntoObject(data) {
-    let newData = {
-      cloud: data.current.cloud,
-      condition: data.current.condition.text,
-      feelslike_c: data.current.feelslike_c,
-      feelslike_f: data.current.feelslike_f,
-      temp_c: data.current.temp_c,
-      temp_f: data.current.temp_f,
-      uv: data.current.uv,
-      wind_kph: data.current.wind_kph,
-      wind_mph: data.current.wind_mph,
-      location_name: data.location.name,
-      country: data.location.country,
-      time: data.location.localtime,
-    };
-    return this.changeText(newData);
-  }
-
-  changeText(data) {
-    const location = document.querySelector(".location");
-    console.log(data);
-    location.textContent = data.location_name;
-
-    const cloud = document.querySelector(".cloud");
-    cloud.textContent = "cloud: " + data.cloud;
-
-    const condition = document.querySelector(".condition");
-    condition.textContent = "condition: " + data.condition;
-
-    const feelsLike = document.querySelector(".feels-like");
-    feelsLike.textContent = "feels like: " + data.feelslike_f;
-  }
-}
-
 const searchFromForm = (() => {
   const form = document.querySelector("form");
   const input = document.querySelector("input");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const weather = new Weather();
-    weather.searchWeather(input.value);
-    // searchWeather(input.value);
+    searchWeather(input.value);
   });
 })();
 
-// async function defaultSearch() {
-//   try {
-//     const weatherData = await fetch(
-//       `http://api.weatherapi.com/v1/current.json?key=61a80c65e68244318ae200949231005&q=new york`
-//     );
-//     const formattedData = await weatherData.json();
-//     dataIntoObject(formattedData);
-//   } catch (err) {
-//     throw new Error(err);
-//   }
-// }
+async function defaultSearch() {
+  try {
+    const weatherData = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=61a80c65e68244318ae200949231005&q=new york&days=5`,
+      { mode: "cors" }
+    );
+    const formattedData = await weatherData.json();
+    dataIntoObject(formattedData);
+  } catch (err) {
+    throw new Error(err);
+  }
+}
 
-// async function searchWeather(search) {
-//   try {
-//     const weatherData = await fetch(
-//       `http://api.weatherapi.com/v1/current.json?key=61a80c65e68244318ae200949231005&q=${search}`
-//     );
-//     const formattedData = await weatherData.json();
-//     dataIntoObject(formattedData);
-//   } catch (err) {
-//     defaultSearch();
-//     throw new Error(err);
-//   }
-// }
+async function searchWeather(search) {
+  try {
+    const weatherData = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=61a80c65e68244318ae200949231005&q=${search}&days=5`,
+      { mode: "cors" }
+    );
+    const formattedData = await weatherData.json();
+    dataIntoObject(formattedData);
+  } catch (err) {
+    defaultSearch();
+    throw new Error(err);
+  }
+}
 
-// function dataIntoObject(data) {
-//   let newData = {
-//     cloud: data.current.cloud,
-//     condition: data.current.condition.text,
-//     feelslike_c: data.current.feelslike_c,
-//     feelslike_f: data.current.feelslike_f,
-//     temp_c: data.current.temp_c,
-//     temp_f: data.current.temp_f,
-//     uv: data.current.uv,
-//     wind_kph: data.current.wind_kph,
-//     wind_mph: data.current.wind_mph,
-//     location_name: data.location.name,
-//     country: data.location.country,
-//     time: data.location.localtime,
-//   };
-//   return changeText(newData);
-// }
+function dataIntoObject(data) {
+  const forecast = data.forecast.forecastday;
+  const location = data.location;
+  // displayWeather(locationDataIntoObj(location), forecastDataIntoObj(forecast));
+  let weather = collectAllData(
+    locationDataIntoObj(location),
+    forecastDataIntoObj(forecast)
+  );
+  displayContent(weather);
+}
 
-// function changeText(data) {
-//   const location = document.querySelector(".location");
-//   console.log(data);
-//   location.textContent = data.location_name;
+function locationDataIntoObj(locationData) {
+  let location = {
+    country: locationData.country,
+    city: locationData.name,
+    region: locationData.region,
+  };
+  return location;
+}
 
-//   const cloud = document.querySelector(".cloud");
-//   cloud.textContent = "cloud: " + data.cloud;
+function forecastDataIntoObj(forecastArray) {
+  let forecastContent = [];
+  forecastArray.forEach((forecast) => {
+    let days = {
+      date: forecast.date,
+      avgtemp_c: forecast.day.avgtemp_c,
+      avgtemp_f: forecast.day.avgtemp_f,
+      condition: forecast.day.condition.text,
+      maxtemp_f: forecast.day.maxtemp_f,
+      maxtemp_c: forecast.day.maxtemp_c,
+    };
+    forecastContent.push(days);
+  });
+  return forecastContent;
+}
 
-//   const condition = document.querySelector(".condition");
-//   condition.textContent = "condition: " + data.condition;
+function collectAllData(locationInfo, weatherInfo) {
+  let allData = { location: locationInfo, weather: weatherInfo };
+  return allData;
+}
 
-//   const feelsLike = document.querySelector(".feels-like");
-//   feelsLike.textContent = "feels like: " + data.feelslike_f;
-// }
+function displayContent(data) {
+  console.log(data);
+  //location
+  const city = document.querySelector(".city");
+  const country = document.querySelector(".country");
+  const state = document.querySelector(".state");
+  city.textContent = data.location.city;
+  country.textContent = data.location.country;
+  state.textContent = data.location.region;
+
+  //forecast
+  let weather = data.weather;
+  weather.forEach((weather) => {
+    const weatherHolder = document.querySelector(".weather");
+
+    const eachWeather = document.createElement("div");
+    weatherHolder.appendChild(eachWeather);
+
+    const date = document.createElement("div");
+    date.textContent = "Today: " + weather.date;
+
+    const tempCelc = document.createElement("div");
+    tempCelc.textContent = "Average Temp Celcius: " + weather.avgtemp_c;
+
+    const maxTempCelc = document.createElement("div");
+    maxTempCelc.textContent = "Max Temp Celcius: " + weather.maxtemp_c;
+
+    const condition = document.createElement("div");
+    condition.textContent = "condition: " + weather.condition;
+
+    eachWeather.append(date, tempCelc, maxTempCelc, condition);
+  });
+}
